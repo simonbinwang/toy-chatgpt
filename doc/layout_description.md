b5c5dc225df36a14a64d72441a899cea70da51fa
Simon Bin Wang <wangsimon@microsoft.com>
2023-04-08 22:42:00 +0800

    Update layout

---
+++ doc/layout_description.md
@@ -0,0 +1,5 @@
+I want to build a app with layout meet some requirements.
+The layout have left and right two part, left part 30% width, right 70%.
+The layout should fill the full height and width of the window.
+It's built with MUI.
+The right part have a textfield at the bottom of the window, which have full width.
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { UserInput } from "./components/UserInput";
-import { Container } from "@mui/material";
 import { UserMessage } from "./components/UserMessage";
 import { BotMessage } from "./components/BotMessage";
 import { observer } from "mobx-react-lite";
@@ -10,36 +9,89 @@ import { useInitConfig } from "./hooks/useInitConfig";
 import { ConfigForm } from "./components/ConfigForm";
 import { configStore } from "./store/ConfigStore";
 import { applySnapshot, getSnapshot } from "mobx-state-tree";
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  Grid,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#303030", // Change this value to adjust the background color
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            backgroundColor: "#4a4a4a", // Change this value to adjust the TextField background color
+          },
+        },
+      },
+    },
+  },
+});
 
 function App() {
   const ready = useInitConfig();
 
   return (
-    <Container>
-      {ready ? (
-        <ConfigForm
-          defaultValue={getSnapshot(configStore)}
-          onChange={(values) => {
-            applySnapshot(configStore, values);
-          }}
-        />
-      ) : null}
-      <UserInput
-        onChange={(message) => {
-          chatEngine.addUserMessage(message);
-        }}
-      />
-      {messageStore.messages.map((message) => {
-        return message.role === "user" ? (
-          <UserMessage message={message.content} />
-        ) : (
-          <BotMessage
-            message={message.content}
-            isCompleted={message.isCompleted}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid container sx={{ height: "100vh", width: "100vw" }}>
+        <Grid item xs={3} sx={{ height: "100%", borderRight: "solid 1px" }}>
+          {ready ? (
+            <ConfigForm
+              defaultValue={getSnapshot(configStore)}
+              onChange={(values) => {
+                applySnapshot(configStore, values);
+              }}
+            />
+          ) : null}
+        </Grid>
+        <Grid
+          item
+          xs={9}
+          container
+          direction="column"
+          justifyContent="space-between"
+          sx={{ height: "100%", position: "relative" }}
+        >
+          <Box>
+            {messageStore.messages.map((message) => {
+              return message.role === "user" ? (
+                <UserMessage message={message.content} />
+              ) : (
+                <BotMessage
+                  message={message.content}
+                  isCompleted={message.isCompleted}
+                />
+              );
+            })}
+          </Box>
+          <UserInput
+            textFieldProps={{
+              variant: "outlined",
+              placeholder: "Type here...",
+              sx: {
+                width: "100%",
+                position: "absolute",
+                bottom: 0,
+              },
+            }}
+            onChange={(message) => {
+              chatEngine.addUserMessage(message);
+            }}
           />
-        );
-      })}
-    </Container>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
